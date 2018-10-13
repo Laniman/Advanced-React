d3d65d5c152e2796804c9b77273199cdf6c7d27d
george <georgij-net@yandex.ru>
2018-10-13 23:01:01 +0300

    add first query and mutation

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,11 @@
-const Query = {};
+const Query = {
+    dogs(parent, args, ctx, info) {
+        return [{
+            name: 'Sunny',
+        }, {
+            name: 'Dog',
+        }];
+    }
+};
 
 module.exports = Query;
