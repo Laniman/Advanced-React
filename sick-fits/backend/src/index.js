77842beb68530b79319910dca1c9514710f14b27
george <georgij-net@yandex.ru>
2018-10-13 17:21:50 +0300

    add graphql yoga server

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,22 @@
-// let's go!
+require('dotenv').config({
+    path: 'variables.env',
+});
+
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO: Use middleware to handle cookies (JWT)
+// TODO: Use middleware to populate current user
+
+server.start({
+   cors: {
+       credential: true,
+       origin: process.env.FRONTED_URL,
+   },
+}, deets => {
+    console.log(`Start on ${deets.port}`);
+});
+
+
