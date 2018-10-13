104c5940662883991a4d72e4ab33c4196cd36a1a
george <georgij-net@yandex.ru>
2018-10-13 23:43:25 +0300

    add items creation

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,11 +1,11 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-    dogs(parent, args, ctx, info) {
-        return [{
-            name: 'Sunny',
-        }, {
-            name: 'Dog',
-        }];
-    }
+    // async items(parent, args, ctx, info) {
+    //     const items = await ctx.db.query.items();
+    //     return items;
+    // }
+    items: forwardTo('db'),
 };
 
 module.exports = Query;
